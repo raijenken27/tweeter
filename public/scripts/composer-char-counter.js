9ec8f6157dd3a5be2ffb438ca54e52c6b9b3142b
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-11-14 11:03:42 -0700

    Adding comments on JS file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,27 @@
+// This script, executed when the DOM has fully loaded, monitors the input events on all textarea elements.
 $(document).ready(function() {
-  //use input event handler
-  $('#tweet-text').on('input', function() {
-    const charLimit = 140;
-    const characters = $(this).val().length;
-    $(this).siblings('div').children('#counter').val(charLimit - characters);
+  $("textarea").on('input', function() {
+    // 'tweetElement' represents the current textarea element, and 'tweetObject' is its corresponding jQuery object.
+    const tweetElement = this;
+    const $var = $(tweetElement); // Rename 'tweetObject' to '$var' for consistency.
+    
+    // Calculate the length of the text within the textarea and update the remaining character count.
+    const tweetLength = $var.val().length;
+    let remainingChars = 140 - tweetLength;
 
-  
-    if (characters > charLimit) {
-      $('#counter').addClass('negative');
-    } else {
-      $('#counter').removeClass('negative');
+    // Access the counter by traversing the DOM tree, starting from the textarea's parent.
+    const formObject = $var.parent();
+    const counterObject = formObject.find(".counter");
+
+    // Set the counter's value based on the remaining characters.
+    counterObject.text(remainingChars);
+
+    // Check and apply styling based on the remaining character count.
+    if (remainingChars < 0) {
+      counterObject.addClass("long");
+    }
+    if (remainingChars >= 0) {
+      counterObject.removeClass("long");
     }
   });
 });
