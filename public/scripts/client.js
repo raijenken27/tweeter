56a3f90822212e869a64d91ef73d327189cd2f78
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-11-14 17:36:20 -0700

    updating JS files + adding asfalt effect

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,102 +3,128 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(() => {
+/**
+ * cross site scripting escape function definition
+ * @param {String} str 
+ * @returns 
+ */
+const safeHTML = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-  // Function to render tweets on the page
-  const renderTweets = function(tweets) {
-    // Clear the tweets container to refresh it
-    $(".tweets-container").empty();
-    // Loop through tweets and append each one to the container
-    tweets.forEach(tweet => {
-      const tweetElement = createTweetElement(tweet);
-      $(".tweets-container").prepend(tweetElement);
-    });
-  };
+/**
+ * Document Dot Ready function
+ */
+$(document).ready(function () {
 
-  // Function to create an HTML element for a given tweet object
-  const createTweetElement = function(tweet) {
-    // Escape function to prevent XSS attacks via tweet.content.text
-    const escape = function(str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-
-    // Construct the tweet HTML element
-    let $tweet = $(`
-      <article class="tweet">
-        <header>
-          <span>
-            <img src=${tweet.user.avatars}>
-            &nbsp${tweet.user.name}
-          </span>
-          <small>
-            ${tweet.user.handle}
-          </small>
-        </header>
-        <p>
-          ${escape(tweet.content.text)}
-        </p>
-        <footer>
-          <div>
-            ${timeago.format(tweet.created_at)}
-          </div>
-          <div class="action">
-            <i class="fa-solid fa-flag"></i> &nbsp; 
-            <i class="fa-solid fa-retweet"></i> &nbsp;
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>
-      </article>
-    `);
+  /**
+   * createTweetElement function definition
+   * @param {Object} tweetObj 
+   * @returns 
+   */
+  const createTweetElement = function (tweetObj) {
+    const $tweet = $(`
+  <article class="tweet">
+          <header>
+            <ul>
+              <div class="user-info">
+                <li class="user-picture"><img src="${tweetObj.user.avatars}" alt="beautiful face avatar" /></li>
+                <li class="user-name">${tweetObj.user.name}</li>
+              </div>
+              <li class="tweeter-handle">${tweetObj.user.handle}</li>
+            </ul>
+          </header>
+          <textarea name="text" readonly id="tweet-text">${safeHTML(tweetObj.content.text)}</textarea>
+          <footer>
+            <ul>
+              <li>${timeago.format(tweetObj.created_at)}</li>
+              <div class="icons">
+                <li><i class="fa-solid fa-flag"></i></li>
+                <li><i class="fa-solid fa-retweet"></i></li>
+                <li><i class="fa-sharp fa-solid fa-heart"></i></li>
+              </div>
+            </ul>
+          </footer>
+  </article>
+  `);
     return $tweet;
+  }
+  /**
+   * renderTweets function definition
+   * @param {Array} tweets 
+   */
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $(".tweet-container").prepend($tweet);
+    }
   };
 
-  // Function to load tweets from the server (/tweets/) and render them
-  const loadTweets = function() {
-    $.ajax('/tweets/', { method: 'GET' })
-      .done(function(data) {
-        renderTweets(data);
-      });
-  };
-
-  // Form submission handler
-  $(function() {
-    const $form = $('.new-tweet form');
-    $form.on('submit', function(event) {
-      event.preventDefault();
-      const queryString = $(this).serialize();
-      const charCounter = $(".counter");
-      const tweetLength = $(charCounter).html();
-
-      // Check for invalid tweet length
-      if (tweetLength >= 140) {
-        $("#error").slideUp(() => {
-          $("#error").html("\⚠️ Cannot post an empty tweet! \⚠️").slideDown();
-        });
-        return;
-      }
-      if (tweetLength < 0) {
-        $("#error").slideUp(() => {
-          $("#error").html("\⚠️ Maximum tweet length exceeded! \⚠️").slideDown();
-        });
-        return;
-      }
-
-      // Send tweet to the server
-      $.ajax('/tweets/', { method: 'POST', data: queryString })
-        .done(() => {
-          // After posting to the server: reload tweets, clear form, reset counter, and hide error
-          loadTweets();
-          $("textarea").val("");
-          $(".counter").html('140');
-          $("#error").slideUp();
-        });
+  /**
+   * loadTweets function definition
+   */
+  const loadTweets = function () {
+    const url = "/tweets";
+    $.get(url, function (data) {
+      renderTweets(data);
     });
+  }
+  
+  // Hide error message if no error and/or tweeth length within limit.
+  $("#tweet-text").on("focus", function (event) {
+    const textValue = $(this).val();
+    const tweetLength = textValue.length;
+    const errorMessage = $(".error-message").css("display");
+
+    if (tweetLength <= 140) {
+      if (errorMessage !== "none") {
+        $(".error-message").slideUp("slow");
+        return;
+      }
+    }
   });
 
-  // Initial load of tweets
+  ///////////////////////////////////
+  // tweet form submission using AJAX
+  ///////////////////////////////////
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    const url = "/tweets/";
+    const postData = $(this).serialize();
+    const textValue = $(this).find("#tweet-text").val();
+    const tweetLength = textValue.length;
+
+    // at the start no error message
+    $errorMessage = $(".error-message").css("display", "none");
+
+    if (!textValue) {
+      $errorMessage.text("You cannot post an empty tweet!");
+      $(".new-tweet").prepend($errorMessage);
+      $errorMessage.slideDown("slow");
+      return;
+    }
+    if (tweetLength > 140) {
+      $errorMessage.text("Your tweet is exceeding the max charac limit!");
+      $(".new-tweet").prepend($errorMessage);
+      $errorMessage.slideDown("slow");
+      return;
+    }
+    $.post(url, postData)
+      .then(function () {
+        //empty out the previous hardcoded starting tweets
+        $(".tweet-container").empty();
+        loadTweets();
+        //clear textbox and counter
+        $("form").find("#tweet-text").val("");
+        $(".counter").val(140);
+      });
+  });
   loadTweets();
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,13 @@
-// This script, executed when the DOM has fully loaded, monitors the input events on all textarea elements.
 $(document).ready(function() {
-  $("textarea").on('input', function() {
-    // 'tweetElement' represents the current textarea element, and 'tweetObject' is its corresponding jQuery object.
-    const tweetElement = this;
-    const $var = $(tweetElement); // Rename 'tweetObject' to '$var' for consistency.
+  $("#tweet-text").on("keyup", function() {
     
-    // Calculate the length of the text within the textarea and update the remaining character count.
-    const tweetLength = $var.val().length;
-    let remainingChars = 140 - tweetLength;
+    const maxTweetLength = 140;
+    const counterDomMarker = $(this).siblings("#tweet-submit").children(".counter");
+    let currentTweetLength = $(this).val().length;
+    let charsAvailable = maxTweetLength - currentTweetLength;
+    
+    counterDomMarker.text(charsAvailable);
 
-    // Access the counter by traversing the DOM tree, starting from the textarea's parent.
-    const formObject = $var.parent();
-    const counterObject = formObject.find(".counter");
-
-    // Set the counter's value based on the remaining characters.
-    counterObject.text(remainingChars);
-
-    // Check and apply styling based on the remaining character count.
-    if (remainingChars < 0) {
-      counterObject.addClass("long");
-    }
-    if (remainingChars >= 0) {
-      counterObject.removeClass("long");
-    }
+    charsAvailable <= 0? counterDomMarker.addClass("counter-negative"): counterDomMarker.removeClass("counter-negative"); 
   });
 });
